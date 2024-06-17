899e5dee4d8df5b1320e029ae2be52e2dd45544e
Tinkerer_Red <48543299+tinkerer-red@users.noreply.github.com>
2024-06-16 19:23:28 -0500

    Update config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {
+    favicon:"assets/docsIcon.png",
+  themeColor:"#11DD11",
+  description:"GML Promise Documentation",
+  latestVersion:"1.0",
+  name:"GML Promise Documentation",
+  otherVersions:[],
+};
